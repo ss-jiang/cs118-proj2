fde2770412744a542c7f22a911503a86c5197022
vagrant <vagrant@client>
2017-05-05 22:15:14 +0000

    added proj1 code to start with

--- client.cpp
+++ client.cpp
@@ -1,8 +1,154 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netdb.h>
+
+#include <stdlib.h>
+#include <fcntl.h>
+
 #include <string>
 #include <thread>
 #include <iostream>
+#include <signal.h>
+#include <sstream>
+#include <fstream>
 
-int main()
+#define MAX_PATH_LENGTH        4096
+
+int main(int argc, char* argv[])
 {
-  std::cerr << "client is not implemented yet" << std::endl;
+  if (argc != 4) {
+    std::cerr << "ERROR: client must be called with exactly three parameters: <HOSTNAME-OR-IP> <PORT> <FILENAME>" <<std::endl;
+    exit(-1);
+  }
+
+  std::string ip_addr = argv[1];
+  int port_num = atoi(argv[2]);
+  std::string file_name = argv[3];
+  
+  // for timeout
+  fd_set myset;
+  int valopt;
+  struct timeval tv;
+  socklen_t lon;
+  long arg; 
+
+  // Check that the port number is in range
+  if (port_num < 1024 || port_num > 65535) {
+    std::cerr << "ERROR: Port number out of range [1024 - 65535]" << std::endl;
+    exit(1);
+  }
+
+  // create a socket using TCP IP
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  // set non-blocking to check timeout
+  arg = fcntl(sockfd, F_GETFL, NULL); 
+  arg |= O_NONBLOCK; 
+  fcntl(sockfd, F_SETFL, arg); 
+
+  // stuff to get address information and connect
+  struct addrinfo hints, *res;
+  int status;
+  char ipstr[INET_ADDRSTRLEN] = {'\0'};
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_flags = AI_PASSIVE; 
+  hints.ai_family = AF_INET; // AF_INET specifies IPv4
+  hints.ai_socktype = SOCK_STREAM;
+
+  // e.g. "www.example.com" or IP; e.g. "http" or port number
+  if ((status = getaddrinfo(argv[1], argv[2], &hints, &res)) != 0) {
+    std::cerr << "ERROR: getaddrinfo: " << gai_strerror(status) << std::endl;
+    exit(1);
+  }
+
+  // TIMEOUT FROM http://developerweb.net/viewtopic.php?id=3196
+  int cv = connect(sockfd, res->ai_addr, res->ai_addrlen);
+  if (cv < 0) { 
+    if (errno == EINPROGRESS) { 
+      while(1) 
+      { 
+        tv.tv_sec = 10; 
+        tv.tv_usec = 0; 
+        FD_ZERO(&myset); 
+        FD_SET(sockfd, &myset); 
+
+        cv = select(sockfd+1, NULL, &myset, NULL, &tv); 
+        if (cv < 0 && errno != EINTR) { 
+          std::cerr << "ERROR: Error connecting\n";
+          exit(1);
+        } 
+        else if (cv > 0) { 
+          // Socket selected for write 
+          lon = sizeof(int); 
+          if (getsockopt(sockfd, SOL_SOCKET, SO_ERROR, (void*)(&valopt), &lon) < 0) { 
+            std::cerr << "ERROR: Error in getsockopt()\n";
+            exit(1); 
+          } 
+          if (valopt) { 
+            std::cerr << "ERROR: Error in delayed connection()\n"; 
+            exit(1); 
+          } 
+          break; 
+        } 
+        else 
+        { 
+          std::cerr << "ERROR: Timeout while connecting\n"; 
+          exit(1); 
+        } 
+      } 
+    } 
+    else 
+    { 
+      std::cerr << "ERROR: Error connecting\n";
+      exit(1); 
+    } 
+  } 
+  // Set to blocking mode again... 
+  arg = fcntl(sockfd, F_GETFL, NULL); 
+  arg &= (~O_NONBLOCK); 
+  fcntl(sockfd, F_SETFL, arg); 
+
+  struct sockaddr_in clientAddr;
+  socklen_t clientAddrLen = sizeof(clientAddr);
+  if (getsockname(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen) == -1) {
+    std::cerr << "ERROR: Failed to get socket name" << std::endl;
+    exit(1);
+  }
+  sleep(20);
+  inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
+  std::cout << "Set up a connection from: " << ipstr << ":" << ntohs(clientAddr.sin_port) << std::endl;
+
+  std::ifstream open_file (file_name.c_str(), std::ios::in | std::ios::binary );
+  char in_buffer[1500];
+  int wc = 0;
+
+  while(!open_file.eof())
+  {
+    open_file.read(in_buffer, 1500);
+    int sent = send(sockfd, in_buffer, open_file.gcount(), 0);
+    if (sent > 0)
+    {
+      wc += sent;
+    }
+    if (sent == -1)
+    {
+      std::cerr << "ERROR: Could not send file\n";
+      exit(1); 
+    }
+  }
+  std::cout << "Sent file: " << file_name << std::endl;
+  std::cout << "Bytes: " << wc << std::endl;
+
+  open_file.close();
+  close(sockfd);
+
+  return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,8 +1,187 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <fcntl.h> 
+#include <signal.h>
+#include <fstream>
+
 #include <string>
 #include <thread>
 #include <iostream>
 
-int main()
+// server is called with the following parameters
+// server <PORT> <FILE-DIR>
+
+bool SIG_HANDLER_CALLED = 0;
+
+void signal_handler(int signum)
 {
-  std::cerr << "server is not implemented yet" << std::endl;
+	SIG_HANDLER_CALLED = 1;
+	std::cout << "Signal handler called" << std::endl;
+	exit(0);
+}
+
+void handle_thread(struct sockaddr_in clientAddr, int clientSockfd, int connection_number, std::string file_dir)
+{
+	char ipstr[INET_ADDRSTRLEN] = {'\0'};
+
+	inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
+	std::cout << "Accept a connection from: " << ipstr << ":" << ntohs(clientAddr.sin_port) << std::endl;
+
+	std::string save_name = file_dir + "/" + std::to_string(connection_number) + ".file";
+
+	// Set to blocking mode again... 
+	long arg = fcntl(clientSockfd, F_GETFL, NULL); 
+	arg &= (~O_NONBLOCK); 
+	fcntl(clientSockfd, F_SETFL, arg); 
+
+	// timeout variables
+	fd_set readfds;
+	struct timeval tv;
+	FD_ZERO(&readfds);
+	FD_SET(clientSockfd, &readfds);
+	tv.tv_sec = 10;
+	int rv = select(clientSockfd+1, &readfds, NULL, NULL, &tv);
+
+	if (rv == -1) 
+	{
+		std::cerr << "ERROR: Select() failure" << std::endl;
+	    exit(1);
+	} 
+	else if (rv == 0) 
+	{
+		// variables used to open and write to a file
+		std::ofstream new_file;
+		new_file.open(save_name, std::ios::out | std::ios::binary);
+
+		//std::string err = "ERROR";
+		new_file.write("ERROR", 5);
+	    std::cerr << "Timeout occurred:  No data after 10 seconds\n";
+	} 
+	else 
+	{
+		// variables used to open and write to a file
+		std::ofstream new_file;
+		new_file.open(save_name, std::ios::out | std::ios::binary);
+		char receive_buf[1024] = {0};
+		int file_size = 0;
+		int rc = 0;
+
+		/* get the file name from the client */
+	    while( (rc = recv(clientSockfd, receive_buf, sizeof(receive_buf), 0)) > 0)
+	    {
+		    if (rc == -1) {
+		    	std::cerr << "ERROR: recv() failed\n";
+				exit(1);
+		    }
+
+		    new_file.write(receive_buf, rc);
+		    file_size += rc;
+		    memset(receive_buf, 0, sizeof(receive_buf));
+		}
+
+		std::cout << "Saved file as " << save_name << " : " << file_size << " bytes\n";
+		file_size = 0;
+		new_file.close();
+	}
+
+	close(clientSockfd);
+}
+
+
+int main(int argc, char* argv[])
+{
+	if (argc < 3) {
+      	std::cerr << "ERROR: server must be called with exactly two parameters: <PORT> <FILE-DIR>" << std::endl;
+      	exit(-1);
+   	}
+
+   	// Handle signals
+   	struct sigaction act;
+   	act.sa_handler = &signal_handler;
+   	sigaction(SIGQUIT, &act, NULL);
+   	sigaction(SIGTERM, &act, NULL);
+   	// FOR TESTING PURPOSES: Ctrl-C
+   	// sigaction(SIGINT, &act, NULL);
+
+	
+   	int port_num = atoi(argv[1]);
+   	std::string file_dir = argv[2];
+   	struct addrinfo hints, *res;
+    int status;
+
+   	// Check that the port number is in range
+   	if (port_num < 1024 || port_num > 65535) {
+	    std::cerr << "ERROR: Port number out of range [1024 - 65535]" << std::endl;
+	    exit(1);
+	}
+
+
+	memset(&hints, 0, sizeof(hints));
+    hints.ai_flags = AI_PASSIVE; 
+    hints.ai_family = AF_INET; // AF_INET specifies IPv4
+    hints.ai_socktype = SOCK_STREAM;
+
+    // e.g. "www.example.com" or IP; e.g. "http" or port number
+	if ((status = getaddrinfo(NULL, argv[1], &hints, &res)) != 0) {
+        std::cerr << "ERROR: getaddrinfo: " << gai_strerror(status) << std::endl;
+        exit(1);
+    }
+
+	// create a socket using TCP IP
+	int sockfd = socket(res->ai_family, res->ai_socktype, 0);
+	if (sockfd == -1) {
+		std::cerr << "ERROR: Failed to create socket" << std::endl;
+		exit(-1);
+	}
+
+	// allow others to reuse the address
+	int yes = 1;
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+		perror("setsockopt");
+		exit(1);
+	}
+
+	// bind the socket
+	if (bind(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
+		std::cerr << "ERROR: Failed to bind to socket" << std::endl;
+		exit(-1);
+	}
+
+	// set socket to listen status
+	if (listen(sockfd, 1) == -1) {
+		std::cerr << "ERROR: Failed to listen to socket" << std::endl;
+		exit(-1);
+	}
+
+	// accept a new connection
+	struct sockaddr_in clientAddr;
+	socklen_t clientAddrSize = sizeof(clientAddr);
+	int clientSockfd;
+	int connection_number = 0;
+
+	while ((clientSockfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientAddrSize)) && !SIG_HANDLER_CALLED) 
+	{
+
+		if (clientSockfd == -1) {
+			std::cerr << "ERROR: Failed to get accept connection" << std::endl;
+			exit(1);
+		}
+
+		// Create a thread for each new accepted fd
+		connection_number++;
+		std::thread(handle_thread, clientAddr, clientSockfd, connection_number, file_dir).detach();
+	}
+
+	close(sockfd);
+	close(clientSockfd);
+
+   	exit(0);
 }
